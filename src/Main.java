8236bfa66e24f52c739d839b90ba5c27c49598fb
LeHuy <levanhuy119955@gmail.com>
2022-05-01 11:11:37 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.SortedMap;
+
+public class Main {
+    public static void main(String[] args) {
+//        Cho 2 số nguyên a và b, thực hiện các phép tính số học với a và b và in ra màn hình kết quả:
+//        a+b
+        int Tong = interger(40, 20);
+        System.out.println("Ket qua tong phep tinh a + b = " + Tong);
+
+//            a-b
+        int Tong1 = interger1(40, 30);
+        System.out.println("Ket qua tong phep tinh a - b = " + Tong1);
+
+//        b-a
+        int Tong2 = interger2(10, 30);
+        System.out.println("Ket qua tong phep tinh b - a = " + Tong2);
+
+//        a*b
+        int Tong3 = interger3(10, 30);
+        System.out.println("Ket qua tong phep tinh a * b = " + Tong3);
+
+//        a/b
+        int Tong4 = interger4(60, 2);
+        System.out.println("Ket qua tong phep tinh a / b = " + Tong4);
+
+//        Viết method truyền vào cân nặng (weight) và chiều cao (height). Tính chỉ số BMI với giá trị cân nặng và chiều cao đã cho
+        double Bmi = bmi (80.3, 1.85 );
+        System.out.println("Ket qua BMI = " + Bmi);
+    }
+
+
+    public static int interger(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static int interger1(int a, int b) {
+        int sum1 = a - b;
+        return sum1;
+    }
+    public static int interger2(int a, int b) {
+        int sum2 = b - a;
+        return sum2;
+    }
+    public static int interger3(int a, int b) {
+        int sum3 = a*b;
+        return sum3;
+    }
+    public static int interger4(int a, int b) {
+        int sum4 = a/b;
+        return sum4;
+    }
+    public static double bmi (double weight, double height) {
+        double Bmi = weight/(height*height);
+        return Bmi;
+    }
+}
